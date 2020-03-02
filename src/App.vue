f0d2510b0ab6d428be18f847bf622adef7363278
dm1annem <300225@mail.ru>
2020-03-02 11:46:36 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    
+    
+  </div>
+</template>
+
+<script>
+import lendingWrapper from './components/lending-wrapper'
+
+export default {
+  name: 'App',
+  components: {
+    lendingWrapper,
+    
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/lending-wrapper.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="lending-wrapper">
+    
+  </div>
+</template>
+     
+<script>
+  export default {
+    name: 'lending-wrapper',
+    components: {
+        
+    }
+  }
+</script>
+    
+<style>
+
+  
+</style>
